498fe36c8f4d89c18ddf8fb4c94df1e717594232
Sandipan Das <mr.sandipan.das@gmail.com>
2017-09-21 21:35:33 +0200

    ML week5 exercise

--- ex4/ex4/nnCostFunction.m
+++ ex4/ex4/nnCostFunction.m
@@ -63,21 +63,42 @@ Theta2_grad = zeros(size(Theta2));
 %
 
 
+a1 = [ones(m, 1) X];
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+a2 = [ones(m, 1) a2];
+z3 = a2 * Theta2';
+htheta = sigmoid(z3);
+
+for k = 1:num_labels
+    yk = y == k;
+    hthetak = htheta(:, k);
+    Jk = 1 / m * sum(-yk .* log(hthetak) - (1 - yk) .* log(1 - hthetak));
+    J = J + Jk;
+end
 
 
+regularization = lambda / (2 * m) * (sum((Theta1(:, 2:end) .^ 2)(:)) + sum((Theta2(:, 2:end) .^ 2)(:)));
+J = J + regularization;
+
+for t = 1:m
+    for k = 1:num_labels
+        yk = y(t) == k;
+        delta3(k) = htheta(t, k) - yk;
+    end
+    delta2 = Theta2' * delta3' .* sigmoidGradient([1, z2(t, :)])';
+    delta2 = delta2(2:end);
+
+    Theta1_grad = Theta1_grad + delta2 * a1(t, :);
+    Theta2_grad = Theta2_grad + delta3' * a2(t, :);
+end
+
+Theta1_grad = Theta1_grad / m;
+Theta2_grad = Theta2_grad / m;
 
 
-
-
-
-
-
-
-
-
-
-
-
+Theta1_grad(:, 2:end) = Theta1_grad(:, 2:end) + lambda / m * Theta1(:, 2:end);
+Theta2_grad(:, 2:end) = Theta2_grad(:, 2:end) + lambda / m * Theta2(:, 2:end);
 
 
 % -------------------------------------------------------------
